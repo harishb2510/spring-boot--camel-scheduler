3b3d096e067a2700cc692ddc26718f63b1b48b8a
Harish <harish25102000@gmail.com>
2023-07-21 08:44:41 +0530

    tb_product table mapped in model

--- src/main/java/com/example/demo/model/STModel.java
+++ src/main/java/com/example/demo/model/STModel.java
@@ -3,14 +3,39 @@ package com.example.demo.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
-@Table(name = "Smart_tracz_db")
+@Table(name = "tb_product")
 @Data
 public class STModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long product_id;
+    private Timestamp created;
+    private String created_by;
+    private Timestamp modified;
+    private String modified_by;
     private String materialId;
     private String materialName;
+   private String record_owner;
+    private String code;
+    private String description;
+    private Integer level_number;
+    private String name;
+    private String sales_spec;
+    private Boolean visible;
+    private Long base_uom_id;
+    private Long material_type_id;
+    private Long packaging_id;
+    private Long parent_product_id;
+    private Long plant_id;
+    private String record_status;
+    private String uuid;
+    private String date_archived;
+    private Long transportation_group_id;
+    private Long material_group1_id;
+    private Long material_group2_id;
+    private Long material_group3_id;
+    private Long material_group4_id;
 }
